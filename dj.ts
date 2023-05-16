39ce06ca6bd0ed2f60276c60e83ece72c0db1b74
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-16 12:23:12 +0900

    import를 하지 못하는 이유?

--- dj.ts
+++ dj.ts
@@ -1,4 +1,4 @@
-export default function (a: string): string {
+export function daegun(a: string): string {
   const b: string = `hello this is ${a}`;
   return b;
 }
--- test.ts
+++ test.ts
@@ -1,16 +1,10 @@
-import daegun from "./dj";
+import { daegun } from "./dj";
 
 const a = 1;
 const b = 2;
 
-function moreFuction(): void {
-  console.log("hi");
-}
-
-daegun("대건입니다");
-
 function c(a: number, b: number): number {
   return a + b;
 }
-
+console.log(daegun("대건입니다"));
 console.log(c(a, b));
