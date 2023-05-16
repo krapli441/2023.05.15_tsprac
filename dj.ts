6aec443f6be30f786092a1d7baed08019ef7b111
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-16 12:13:40 +0900

    타입스크립트 모듈 기능 만들기

---
+++ dj.ts
@@ -0,0 +1,4 @@
+export default function (a: string): string {
+  const b: string = `hello this is ${a}`;
+  return b;
+}
--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import { data } from "./module-file";
+import daegun from "./dj";
 
 const a = 1;
 const b = 2;
@@ -7,9 +7,10 @@ function moreFuction(): void {
   console.log("hi");
 }
 
+daegun("대건입니다");
+
 function c(a: number, b: number): number {
   return a + b;
 }
 
 console.log(c(a, b));
-console.log(data);
