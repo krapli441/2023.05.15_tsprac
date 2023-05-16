632efd41b62193ba36a258ca644c99eb98ad23ea
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-16 11:21:53 +0900

    타입스크립트를 입력하고 js 파일을 만들었다.

---
+++ test.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+var a = 1;
+var b = 2;
+function c(a, b) {
+    return a + b;
+}
+console.log(c(a, b));
---
+++ test.ts
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+const a = 1;
+const b = 2;
+
+function c(a, b) {
+  return a + b;
+}
+
+console.log(c(a, b));
