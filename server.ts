111c1584d6e62f81629a63c7e19d2555e42eae6c
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-16 16:15:25 +0900

    서버 로직 작성해보기

--- server.ts
+++ server.ts
@@ -2,10 +2,12 @@ import http from "http";
 import fs from "fs";
 import { error } from "console";
 const server = http.createServer((req, rep) => {
-  const page = fs.readFileSync("./index.html");
-  rep.writeHead(200, { "content-type": "text/html; charset=utf-8;" });
-  rep.write(page);
-  rep.end();
+  if (req.url === "/") {
+    const page = fs.readFileSync("./index.html");
+    rep.writeHead(200, { "content-type": "text/html; charset=utf-8;" });
+    rep.write(page);
+    rep.end();
+  }
 });
 
 server.listen(2080, () => {
