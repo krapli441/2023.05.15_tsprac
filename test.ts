ff9f0d6e6fa21ff0a01474acbb23c6d87aa55513
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-17 09:32:31 +0900

    아침 몸풀이

--- test.ts
+++
@@ -1,9 +0,0 @@
-function createElement(tagName: string, props: object) {
-  let propsString = "";
-  for (let key in props) {
-    propsString += ` ${key}=${props[key]}`;
-  }
-  return `<${tagName}${propsString}></${tagName}>`;
-}
-
-console.log(createElement("div", { class: "training", id: "typescript" }));
